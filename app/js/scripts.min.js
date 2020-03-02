94e3dcc9808f48c45c995d0167080bc41e229254
vlados-ctrl <55538429+vlados-ctrl@users.noreply.github.com>
2020-03-02 20:28:01 +0300

    adaptive + script

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -1,5 +1,9 @@
-$(function() {
+$('.menu-btn').on('click', function(e) {
+    e.preventDefault;
+    $(this).toggleClass('menu-btn_active');
+    $('.menu__phone-nav').toggleClass('menu__phone-nav_active');
+});
 
-	// Custom JS
-
+$(document).ready(function() {
+    $("#phone").inputmask("+7 (999) 999-99-99")
 });
